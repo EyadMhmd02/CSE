76d09d3326e79914b98505d4d177d8ed8f310b20
EyadMhmd02 <128830777+EyadMhmd02@users.noreply.github.com>
2025-01-02 20:57:58 +0200

    CSE222_Revision.docx

--- src/y3/s1/Main.java
+++ src/y3/s1/Main.java
@@ -1,70 +1,26 @@
 package y3.s1;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class Main {
     public static void main(String[] args) {
-        List<Character> list = new ArrayList<Character>();
-        for (int i = 0; i < 7; i++) {
-            list.add((char) ('a' + i));
-        }
 
-        System.out.println(list); // [a, b, c, d, e, f, g]
-
-        System.out.println(myReverse(list));
     }
 
-    public static void method1() {
-        int x = 3;
-        System.out.println(x); // 3
-        method2();
-        // null
-        // hello
-        // null
-        System.out.println(x); // 3
-        System.out.println("end"); // end
-    }
-
-    public static void method2() {
-        Integer x = null;
-        System.out.println(x); // null
-        method3(); // hello
-        System.out.println(x); // null
-    }
-
-    public static void method3() {
-        String x = "hello";
-        System.out.println(x); // hello
-    }
-
-    public static void printHelloWorld() {
-        System.out.println("Hello, World!");
-    }
-
-    public static int addFour(int x) {
-        return x + 4;
-    }
-
-    public static double hypotenuse(double length, double width) {
-        return Math.sqrt((length * length) + (width * width));
-    }
-
-    public static String firstNChars(String s, int n, boolean reverseIt) {
-        if (reverseIt) {
-            return new StringBuilder(s).reverse().substring(0, n);
-        } else {
-            return s.substring(0, n);
+    public static void insertionSort(int[] a) {
+        int n = a.length;
+        for (int i = 1; i < n; i++) {
+            for (int j = i; j > 0 && less(a[j], a[j - 1]); j--) {
+                exch(a, j, j - 1);
+            }
         }
     }
 
-    public static <T> List<T> myReverse(List<T> list) {
-        List<T> newList = new ArrayList<>();
+    private static boolean less(Comparable v, Comparable w) {
+        return v.compareTo(w) < 0;
+    }
 
-        for (T x : list) {
-            newList.add(0, x);
-        }
-
-        return newList;
+    private static void exch(int[] a, int i, int j) {
+        int t = a[i];
+        a[i] = a[j];
+        a[j] = t;
     }
 }
