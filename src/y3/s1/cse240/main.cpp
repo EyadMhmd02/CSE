35d4506e8b21e4bf9d8222418b3005269a6e6698
EyadMhmd02 <128830777+EyadMhmd02@users.noreply.github.com>
2025-01-12 20:39:01 +0200

    CSE240_Revision.docx

--- src/y3/s1/cse240/main.cpp
+++ src/y3/s1/cse240/main.cpp
@@ -1,11 +1,7 @@
-//
-// Created by eyado on 03/12/2024.
-//
-
 #include <iostream>
 using namespace std;
 
 int main() {
-    cout <<
-            (1 * 2) - (3 + 4) - (5 / 6) - 7 << endl;
+    int result =
+            (1 * 2) - (3 + 4) - (5 / 6) - 7;
 }
